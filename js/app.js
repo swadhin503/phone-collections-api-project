9020d6749618ee46cf01c7bc578b0c787057ad07
swadhin503 <tawhiddwinswadhin@gmail.com>
2022-03-02 01:43:51 +0600

    phone datails half

--- js/app.js
+++ js/app.js
@@ -6,29 +6,30 @@ const phoneSearching = () => {
     // console.log(searchValue);
     searchInput.value ="";
     
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    fetch(url)
+    const nameUrl = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(nameUrl)
     .then(res => res.json())
     .then(data => searchResult(data.data));
+}
 
-    const searchResult = phones =>{
+const searchResult = phones =>{
         // console.log(phones);
-        const parentDiv = document.getElementById('card-container');
+  const parentDiv = document.getElementById('card-container');
+        parentDiv.textContent='';
 
  if(phones.length > 0){
     for(const phone of phones.slice(0,20)){
-        console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML=`
-        <div class="card">
+        <div class="card h-100">
           <img src="${phone.image}" class="card-img-top p-3" alt="..." />
           <div class="card-body">
             <h3 class="card-title">${phone.phone_name}</h3>
         <h5>Brand Name: ${phone.brand}</h5>
 
             <button
-                onclick="phoneDetails()"
+                onclick="phoneDetails(${phone.slug})"
                 class="btn btn-outline-secondary btn-primary text-white mx-auto"
                 type="button"
                 >
@@ -38,6 +39,7 @@ const phoneSearching = () => {
         </div>
         `;
         parentDiv.appendChild(div);
+
     }
  }
  else{
@@ -48,7 +50,12 @@ const phoneSearching = () => {
     const blankDiv = document.getElementById('blank-result');
     blankDiv.appendChild(div);
     parentDiv.textContent = '';
+  }
  }
- parentDiv.textContent='';
-    }
-}
+
+const phoneDetails = id => {
+    const idUrl = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(idUrl)
+    .then(res => res.json())
+    .then(data => console.log(data));
+ }
